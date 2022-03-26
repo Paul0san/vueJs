e6d8116deab756f76c34e3ce47c6ee100f4e5f7e
Paulo Sanchez <paulocesarsanchezespindola@gmail.com>
2022-03-26 11:00:40 -0600

    before vuex

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <img src="https://cdn.dribbble.com/users/1059583/screenshots/4171367/media/34e69eb61a7bd8dea1c957a8b82605a7.gif" alt="developer" class="center">
+        <h4>
+        Trabajando en el sitio...
+        </h4>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "DashboardView"
+    }
+</script>  
+
+<style scoped>
+    .center {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 50%;
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,170 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <body class="my-login-page">
+      <section class="h-100">
+        <div class="container h-100">
+          <div class="row justify-content-md-center h-100">
+            <div class="card-wrapper">
+              <div class="brand">
+                <img src="@/assets/logo.png" alt="logo">
+              </div>
+              <div class="card fat">
+                <div class="card-body">
+                  <h4 class="card-title">Login</h4>
+                  <form v-on:submit.prevent="login" class="my-login-validation" novalidate="">
+                    <div class="form-group">
+                      <label for="user">Username</label>
+                      <input id="user" type="user" class="form-control" name="user" value="" v-model="user" required autofocus>
+                      <div class="invalid-feedback">
+                        Email is invalid
+                      </div>
+                    </div>
+
+                    <div class="form-group">
+                      <label for="password">Password
+                      </label>
+                      <input id="password" type="password" class="form-control" name="password" v-model="password" required data-eye>
+                        <div class="invalid-feedback">
+                          Password is required
+                        </div>
+                    </div>
+
+                    <div class="form-group m-0 mt-4">
+                      <button type="submit" class="btn btn-primary btn-block">
+                        Login
+                      </button>
+                    </div>
+                    <div class="mt-4 text-center">
+                      ¿No tienes una cuenta? <a href="register.html">Crea una</a>
+                    </div>
+                    <div class="alert alert-danger" role="alert" v-if="error">
+                      {{error_msg}}
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </body>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  // @ is an alias to /src
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  import Axios from 'axios';
+  export default {
+    name: 'HomeView',
+    components: {
+    },
+    data: function(){
+      return {
+        user: "",
+        password: "",
+        error: false,
+        error_msg: "",
+      }
+    },
+    methods:{
+      login(){
+        let jsonCredentials = {
+          "usuario": this.user,
+          "password": this.password
+        };
+
+
+      Axios.post('https://api.solodata.es/auth', jsonCredentials)
+        .then( data => {
+          if(data.data.status === "ok"){
+            localStorage.token = data.data.result.token;
+            this.$router.push('dashboard');
+          }else{
+            this.error = true;
+            this.error_msg = data.data.result.error_msg;
+          }
+        })  
+      }
+    }
   }
-}
 </script>
+
+<style scoped>
+  html,body {
+    height: 100%;
+    background-color: #f7f9fb;
+
+  }
+
+  body.my-login-page {
+    font-size: 14px;
+  }
+
+  .my-login-page .brand {
+    width: 90px;
+    height: 90px;
+    overflow: hidden;
+    border-radius: 50%;
+    margin: 40px auto;
+    box-shadow: 0 4px 8px rgba(0,0,0,.05);
+    position: relative;
+    z-index: 1;
+  }
+
+  .my-login-page .brand img {
+    width: 100%;
+  }
+
+  .my-login-page .card-wrapper {
+    width: 400px;
+  }
+
+  .my-login-page .card {
+    border-color: transparent;
+    box-shadow: 0 4px 8px rgba(0,0,0,.1);
+  }
+
+  .my-login-page .card.fat {
+    padding: 10px;
+  }
+
+  .my-login-page .card .card-title {
+    margin-bottom: 30px;
+  }
+
+  .my-login-page .form-control {
+    border-width: 2.3px;
+  }
+
+  .my-login-page .form-group label {
+    width: 100%;
+  }
+
+  .my-login-page .btn.btn-block {
+    padding: 12px 10px;
+  }
+
+  .my-login-page .footer {
+    margin: 40px 0;
+    color: #888;
+    text-align: center;
+  }
+
+  @media screen and (max-width: 425px) {
+    .my-login-page .card-wrapper {
+      width: 90%;
+      margin: 0 auto;
+    }
+  }
+
+  @media screen and (max-width: 320px) {
+    .my-login-page .card.fat {
+      padding: 0;
+    }
+
+    .my-login-page .card.fat .card-body {
+      padding: 15px;
+    }
+  }
+</style>
